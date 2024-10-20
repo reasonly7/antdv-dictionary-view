da69b6a09a3c77ec194f8083860f15163ff3cd45
reasonly8 <reasonly8@sina.com>
2024-10-20 19:17:39 +0800

    feat: init project

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import DictionaryView from "./views/dictionary-view/DictionaryView.vue";
+</script>
+
+<template>
+  <DictionaryView></DictionaryView>
+</template>
+
+<style scoped lang="less"></style>
